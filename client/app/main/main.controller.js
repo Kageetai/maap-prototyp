980ed01f77aeb5f76c657a9002c4debdeb9b8acb
Kageetai <ma@kageetai.net>
2016-02-21 22:20:47 +0100

    styles for map and specific color for every regarding their source

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,15 +13,44 @@ class MainController {
         latitude: 52.520007,
         longitude: 13.404954
       },
-      zoom: 13
+      zoom: 13,
+      options: {
+        mapTypeControl: false,
+        streetViewControl: false
+      }
     };
 
-    $http.get('/api/events').then(response => {
-      this.events = response.data.map((event) => {
+    $http.get('assets/gmap-styles.json')
+      .then((res) => {
+        this.map.options.styles = res.data;
+      });
+
+    var promises = [
+      $http.get('/api/importers')
+        .then(response => {
+          return this.importers = response.data;
+        }),
+      $http.get('/api/events')
+        .then(response => {
+          this.events = response.data;
+        })
+    ];
+
+    Promise.all(promises).then(() => {
+      this.events.map((event) => {
         event.location.coordinates.latitude = event.location.coordinates.lat;
         event.location.coordinates.longitude = event.location.coordinates.lng;
+        event.icon = {
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 10,
+          strokeWeight: 1,
+          fillColor: this.importers.filter((ele) => {
+            return ele.name.toLowerCase() === event.source.toLowerCase();
+          })[0].color,
+          fillOpacity: 1
+        };
         return event;
-      });
+      })
     });
 
     this.markersEvents = {
