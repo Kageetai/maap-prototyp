18bcf0427415598616202c46dd1e7fc0f9f4ea8a
Kageetai <ma@kageetai.net>
2016-02-22 16:44:52 +0100

    show color of event source on event list

--- client/app/events/events.controller.js
+++ client/app/events/events.controller.js
@@ -12,15 +12,28 @@ class EventsController {
       this.userCoords = coords;
     });
 
-    $http.get('/api/events').then(response => {
-      this.events = response.data.map((event) => {
+    var promises = [
+      $http.get('/api/importers')
+        .then(response => {
+          this.importers = response.data;
+        }),
+      $http.get('/api/events')
+        .then(response => {
+          this.events = response.data;
+        })
+    ];
+
+    Promise.all(promises).then(() => {
+      this.events.map((event) => {
         event.location.coordinates.latitude = event.location.coordinates.lat;
         event.location.coordinates.longitude = event.location.coordinates.lng;
         event.distance = Locator.getDistance(event.location.coordinates.lat, event.location.coordinates.lng);
+        event.color = this.importers.filter((ele) => {
+          return ele.name.toLowerCase() === event.source.toLowerCase();
+        })[0].color;
         return event;
-      });
+      })
     });
-
   }
 }
 
