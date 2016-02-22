f3125e712463a8377539040ae78eab8973b26ad1
Kageetai <ma@kageetai.net>
2016-02-22 11:11:15 +0100

    display and sort by distance of events

--- client/app/events/events.controller.js
+++ client/app/events/events.controller.js
@@ -4,14 +4,19 @@
 
 class EventsController {
 
-  constructor($http) {
+  constructor($http, Locator) {
     this.$http = $http;
+    this.locator = Locator;
     this.events = [];
+    Locator.locate().then((coords) => {
+      this.userCoords = coords;
+    });
 
     $http.get('/api/events').then(response => {
       this.events = response.data.map((event) => {
         event.location.coordinates.latitude = event.location.coordinates.lat;
         event.location.coordinates.longitude = event.location.coordinates.lng;
+        event.distance = Locator.getDistance(event.location.coordinates.lat, event.location.coordinates.lng);
         return event;
       });
     });
