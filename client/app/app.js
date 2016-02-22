5789da2e9c71b6e4ca0c96bb5d88f0db4443a2b9
Kageetai <ma@kageetai.net>
2016-02-22 22:49:42 +0100

    add facebook app id to share button

--- client/app/app.js
+++ client/app/app.js
@@ -22,7 +22,7 @@ angular.module('maapApp', [
   .config(function (uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
       key: 'AIzaSyDxHmFqesCdTNc2e6Q45364edVZgc847qs',
-      v: '3.20', //defaults to latest 3.X anyhow
+      //v: '3.20', //defaults to latest 3.X anyhow
       libraries: 'geometry,visualization'
     });
   });
